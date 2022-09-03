f278c389d73df45bbb93c965ac4e45f5f49fc2b8
PANTORY <pantory@gmail.com>
2022-09-03 16:25:02 +0300

    refactor: replace markup & styles on home page

--- src/components/pages/home/home.ts
+++ src/components/pages/home/home.ts
@@ -7,8 +7,14 @@ class Home {
     const container = createElement('div', { class: 'home-page' });
 
     container.innerHTML = `
+      <h1 class="home-page__title">
+        Н<span class="home-page__title-light">о</span>вого<span class="home-page__title-light">д</span>няя 
+        и<span class="home-page__title-light">г</span>ра
+        «На<span class="home-page__title-light">р</span>яди 
+        <span class="home-page__title-light">ё</span>лку»
+      </h1>
       <a class="home-page__link" href="#/toys">
-        <h1 class="home-page__title">Помогите бабушке нарядить ёлку</h1>
+        <span class="home-page__link-item">Начать</span>
       </a>
       <div class="home-page__overlay"></div>
     `;
