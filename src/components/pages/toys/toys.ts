cbd332662e45a4c57cf1a4b8f4db620d159ef355
PANTORY <pantory@gmail.com>
2022-09-06 13:23:04 +0300

    refactor: add separate method for cards render

--- src/components/pages/toys/toys.ts
+++ src/components/pages/toys/toys.ts
@@ -19,9 +19,30 @@ import decorations from '../../../data/decorations';
 class Toys {
   data;
 
+  cardsContainer;
+
   constructor() {
     this.data = decorations;
+
+    this.cardsContainer = createElement('section', { class: 'toys-page__cards' });
   }
+
+  renderCards = () => {
+    while (this.cardsContainer.firstChild)
+      this.cardsContainer.removeChild(this.cardsContainer.firstChild);
+
+    searchToy(sortData(filterByValue(this.data))).forEach((item) => {
+      this.cardsContainer.append(new ToyCard(item).render());
+    });
+
+    if (!this.cardsContainer.firstChild) {
+      const errorMsg = createElement('p', { class: 'toys-page__error' }, [
+        'Извините, совпадений не обнаружено',
+      ]);
+
+      this.cardsContainer.append(errorMsg);
+    }
+  };
 
   render = () => {
     const page = createElement('div', { class: 'toys-page' });
@@ -34,46 +55,28 @@ class Toys {
     const valueFilters = new ValueFilters(this.data).render();
     const sorting = new Sorting().render();
     const mainContainer = createElement('main', { class: 'toys-page__main' });
-    const cardsContainer = createElement('section', { class: 'toys-page__cards' });
 
-    this.data.forEach((item) => cardsContainer.append(new ToyCard(item).render()));
+    this.renderCards();
 
     snowButton.addEventListener('click', () => {
       snowfall.toggleShow();
     });
 
     search.addEventListener('input', () => {
-      cardsContainer.innerHTML = '';
-
-      searchToy(sortData(filterByValue(this.data))).forEach((item) => {
-        cardsContainer.append(new ToyCard(item).render());
-      });
-
-      if (!cardsContainer.innerHTML)
-        cardsContainer.innerHTML = `
-          <p class="toys-page__error">Извините, совпадений не обнаружено</p>
-        `;
+      this.renderCards();
     });
 
     valueFilters.addEventListener('input', () => {
-      cardsContainer.innerHTML = '';
-
-      searchToy(sortData(filterByValue(this.data))).forEach((item) => {
-        cardsContainer.append(new ToyCard(item).render());
-      });
+      this.renderCards();
     });
 
     sorting.addEventListener('change', () => {
-      cardsContainer.innerHTML = '';
-
-      searchToy(sortData(filterByValue(this.data))).forEach((item) => {
-        cardsContainer.append(new ToyCard(item).render());
-      });
+      this.renderCards();
     });
 
     topLeftWrapper.append(musicButton, snowButton, search);
     leftContainer.append(topLeftWrapper, valueFilters, sorting);
-    mainContainer.append(leftContainer, cardsContainer);
+    mainContainer.append(leftContainer, this.cardsContainer);
     page.append(snowfall.render(), header.render(), mainContainer);
 
     return page;
