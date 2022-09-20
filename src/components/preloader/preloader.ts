c011848ad65c863c3c630ce9d982858b3934aa5a
PANTORY <pantory@gmail.com>
2022-09-20 23:00:25 +0300

    feat: add app preloader

---
+++ src/components/preloader/preloader.ts
@@ -0,0 +1,31 @@
+import createElement from '../../utils/createElement';
+import './preloader.scss';
+
+class Preloader {
+  render = () => {
+    const container = createElement('div', { class: 'preloader' });
+
+    container.innerHTML = `
+      <div class="preloader__inner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    `;
+
+    container.addEventListener('transitionend', () => container.remove());
+
+    return container;
+  };
+}
+
+export default Preloader;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
 import './index.scss';
 
+import Preloader from './components/preloader/preloader';
 import router from './router/router';
 
+const { body } = document;
+const preloader = new Preloader().render();
+
+body.style.overflow = 'hidden';
+body.append(preloader);
+
 window.addEventListener('hashchange', router);
-window.addEventListener('load', router);
+window.addEventListener('load', () => {
+  router();
+
+  setTimeout(() => {
+    body.removeAttribute('style');
+    preloader.classList.add('preloader--hide');
+  }, 2000);
+});
