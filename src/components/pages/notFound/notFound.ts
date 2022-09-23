5007d80808de2525fc71af5ed3bbd1137b36f7df
PANTORY <pantory@gmail.com>
2022-09-23 17:02:39 +0300

    feat: add markup & styles to 404 page

--- src/components/pages/notFound/notFound.ts
+++ src/components/pages/notFound/notFound.ts
@@ -1,13 +1,34 @@
+import './notFound.scss';
+
+import Snowfall from '../../snowfall/snowfall';
+
 import createElement from '../../../utils/createElement';
 
 class NotFound {
   render = () => {
     const container = createElement('div', { class: 'not-found-page' });
+    const snowfall = new Snowfall({}).render();
 
     container.innerHTML = `
-      <h2>Страница не найдена</h2>
+      <div class="not-found-page__inner">
+        <div class="not-found-page__top">
+          <h2 class="not-found-page__title">Страница не существует</h2>
+          <a class="not-found-page__link" href="#/">
+            <div class="not-found-page__link-inner">
+              <span class="not-found-page__link-text">На главную</span>
+              <span class="not-found-page__link-arrow"></span>
+            </div>
+          </a>
+        </div>
+        <div class="not-found-page__bottom">
+          <div class="not-found-page__ground"></div>
+          <h2 class="not-found-page__num">404</h2>
+          <div class="not-found-page__snowdrift"></div>
+        <div>
+      </div>
     `;
 
+    container.append(snowfall);
     return container;
   };
 }
