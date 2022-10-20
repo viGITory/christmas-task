fc6210a204a80b4b9221ab22550e71cca604e160
PANTORY <pantory@gmail.com>
2022-10-20 22:41:18 +0300

    feat: add common components to tree page

--- src/components/pages/tree/tree.ts
+++ src/components/pages/tree/tree.ts
@@ -1,15 +1,31 @@
 import './tree.scss';
 
+import Snowfall from '../../snowfall/snowfall';
 import Header from '../../header/header';
+import MusicButton from '../../musicButton/musicButton';
+import SnowButton from '../../snowButton/snowButton';
 
 import createElement from '../../../utils/createElement';
 
 class Tree {
   render = () => {
-    const container = createElement('div', { class: 'tree-page' });
-    const header = new Header().render();
+    const container = createElement('div', { class: 'tree-page' }, [
+      new Snowfall({ className: 'snowfall--hide' }).render(),
+      new Header().render(),
+    ]);
+    const mainContainer = createElement('main', { class: 'tree-page__main' }, [
+      createElement('h1', { class: 'visually-hidden' }, ['Новогодняя ёлка']),
+    ]);
+    const leftContainer = createElement('section', { class: 'tree-page__left' }, [
+      createElement('h2', { class: 'visually-hidden' }, ['Параментры ёлки']),
+      createElement('div', { class: 'tree-page__controls' }, [
+        new MusicButton().render(),
+        new SnowButton().render(),
+      ]),
+    ]);
 
-    container.append(header);
+    mainContainer.append(leftContainer);
+    container.append(mainContainer);
 
     return container;
   };
